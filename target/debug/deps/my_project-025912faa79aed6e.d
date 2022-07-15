a8257ce51a36c9ba98fb6e94902439eb61e5082b
Amirhossein Davoody <amirhossein.davoody@gmail.com>
2022-07-15 05:22:06 +0000

    Initial commit

---
+++ target/debug/deps/my_project-025912faa79aed6e.d
@@ -0,0 +1,5 @@
+/home/runner/ElasticShadyPortablesoftware/target/debug/deps/my_project-025912faa79aed6e.rmeta: src/main.rs
+
+/home/runner/ElasticShadyPortablesoftware/target/debug/deps/my_project-025912faa79aed6e.d: src/main.rs
+
+src/main.rs:
